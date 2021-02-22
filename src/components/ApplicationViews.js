ff7de6bdf5d83de548f7f2484123b082da66a98d
Leo Rondeau <christopherleorondeau@gmail.com>
2021-02-21 22:18:13 -0600

    added gameLIst to application views

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,5 +1,8 @@
 import React from "react"
 import { Route } from "react-router-dom"
+import { GameProvider } from "./game/GameProvider"
+import { GameList } from "./game/GameList"
+import { GameDetail } from './game/GameDetail'
 
 export const ApplicationViews = () => {
     return <>
@@ -7,7 +10,14 @@ export const ApplicationViews = () => {
             margin: "5rem 2rem",
             backgroundColor: "lightgoldenrodyellow"
         }}>
-            Application views
+            <GameProvider>
+                <Route exact path="/games" render={(props) => {
+                    return <GameList {...props} />
+                }}></Route>
+                <Route exact path="/games/:gameId(\d+)" render={(props) => {
+                        return <GameDetail {...props} />
+                    }} />
+            </GameProvider>
         </main>
     </>
 }
